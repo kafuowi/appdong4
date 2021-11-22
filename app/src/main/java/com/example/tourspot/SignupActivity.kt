0080faa846f5946ad1cbb4f86fc7ab5062c33b36
kafuowi <pressy286@gmail.com>
2021-11-22 19:51:26 +0900

    signin -> signup

--- app/src/main/java/com/example/tourspot/SignupActivity.kt
+++ app/src/main/java/com/example/tourspot/SignupActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tourspot
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -9,13 +10,22 @@ class SignupActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
-        val btn = findViewById<Button>(R.id.signin_button)
-        val authemail = findViewById<EditText>(R.id.signin_email)
-        var password = 0
+        val btn = findViewById<Button>(R.id.signup_button)
+        val authemail = findViewById<EditText>(R.id.signup_email)
+        val authpw = findViewById<EditText>(R.id.signup_password)
+        val authpwch = findViewById<EditText>(R.id.signup_password_check)
+        var password = ""
         var email = ""
 
         btn.setOnClickListener {
             email = authemail.text.toString()
+            password = authpw.text.toString()
+            var passwordCheck = authpwch.text.toString()
+            if(password.equals(passwordCheck)){
+                val intent = Intent(this, MainActivity::class.java)
+
+                startActivity(intent)
+            }
         }
     }
 }
