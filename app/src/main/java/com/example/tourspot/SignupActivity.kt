c312ca46400fcd2a7b2ee2370d22ffd0a8c2f6fc
leehandsub <gustjq7027@naver.com>
2021-11-22 19:33:43 +0900

    버튼 연결

--- app/src/main/java/com/example/tourspot/SignupActivity.kt
+++ app/src/main/java/com/example/tourspot/SignupActivity.kt
@@ -1,11 +1,20 @@
 package com.example.tourspot
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SignupActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        val btn = findViewById<Button>(R.id.signin_button);
+        btn.setOnClickListener({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        })
+
     }
 }
