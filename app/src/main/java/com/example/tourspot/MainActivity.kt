efe500139487b89e7e7567b913c6c49b99a720a0
kafuowi <pressy286@gmail.com>
2021-11-22 18:57:54 +0900

    review 오류수정

--- app/src/main/java/com/example/tourspot/MainActivity.kt
+++ app/src/main/java/com/example/tourspot/MainActivity.kt
@@ -26,7 +26,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         replaceFragment(HomeFragment())
 
         /* tool bar */
